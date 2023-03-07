226c7982dc02317b9b1d15ad88fb9f68241fc598
Monica Björk <m.bjork95@gmail.com>
2023-03-07 08:29:29 +0100

    Base for theme switch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,21 @@
 <script>
     import HamburgerMenu from '../components/atoms/HamburgerMenu.svelte';
-import ScrollButtonDown from '../components/atoms/ScrollButtonDown.svelte';
-import ScrollButtonUp from '../components/atoms/ScrollButtonUp.svelte';
-import '../global.scss';
+    import ScrollButtonDown from '../components/atoms/ScrollButtonDown.svelte';
+    import ScrollButtonUp from '../components/atoms/ScrollButtonUp.svelte';
+    import ThemeSwitch from '../components/molecules/ThemeSwitch.svelte';
+    import '../global.scss';
+    import { theme } from '../stores';
 </script>
+
+<svelte:head>
+    <meta name="color-scheme" content={$theme == 'system' ? 'light dark' : $theme}/> <link rel="stylesheet" href={`/theme/${$theme}.css`} />
+</svelte:head>
 
 <div class="app">
     <header>
         <HamburgerMenu/>
         header
+        <ThemeSwitch/>
     </header>
     <main>
         <ScrollButtonUp/>
