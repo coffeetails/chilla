2cdd3fdb7704c6df9b4d65fc566e98eb9f840911
Monica Björk <m.bjork95@gmail.com>
2023-03-08 16:38:16 +0100

    solved conflicts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,10 @@
 <script>
-<<<<<<< HEAD
     import { settingsIsVisible, numberOfBlobs } from '../stores';
-    import Blob from "../components/atoms/Blob.svelte";
-    import ScrollButtonDown from "../components/atoms/ScrollButtonDown.svelte";
-    import ScrollButtonUp from "../components/atoms/ScrollButtonUp.svelte";
-    import { onMount } from 'svelte';
-    import SectionSticks from '../components/molecules/SectionSticks.svelte';
-=======
-    import { theme, settingsIsVisible } from '../stores';
-    import { onDestroy, onMount } from 'svelte';
-    
     import Blob from "../components/atoms/Blob.svelte";
     import ScrollButtonDown from "../components/atoms/icons/ScrollButtonDown.svelte";
     import ScrollButtonUp from "../components/atoms/icons/ScrollButtonUp.svelte";
->>>>>>> b449c89f442f9d5e749683c55658721c049beda7
+    import SectionSticks from '../components/molecules/SectionSticks.svelte';
+    import { onDestroy, onMount } from 'svelte';
 
     let blue = 'blue';
     let red = 'red';
@@ -23,18 +14,14 @@
     onMount(() => {
         settingsIsVisible.set(true);
         const blobElems = document.querySelectorAll('.blob');
-        numberOfBlobs.set(blobElems.length);
+        numberOfBlobs.set(blobElems);
     });
 
-<<<<<<< HEAD
-    // console.log($numberOfBlobs);
-=======
     function disableVisibleSettings() {
         settingsIsVisible.set(false);
     }
 
     onDestroy(disableVisibleSettings);
->>>>>>> b449c89f442f9d5e749683c55658721c049beda7
 
 </script>
 
--- src/stores.ts
+++ src/stores.ts
@@ -8,12 +8,8 @@ const aboutContent = writable('intro');
 
 const activeMenu = writable(false);
 
-<<<<<<< HEAD
 const numberOfBlobs = writable(Array);
 
-export { theme, settingsIsVisible, aboutContent, activeMenu, numberOfBlobs };
-=======
 const playAudio = writable(false);
 
-export { theme, settingsIsVisible, aboutContent, activeMenu, playAudio };
->>>>>>> b449c89f442f9d5e749683c55658721c049beda7
+export { theme, settingsIsVisible, aboutContent, activeMenu, playAudio, numberOfBlobs };
