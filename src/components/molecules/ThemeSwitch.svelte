8f1fe9b50817a5a22ecea2d5b8658ac47249f94a
Monica Björk <m.bjork95@gmail.com>
2023-03-10 10:25:56 +0100

    updated themeswitcher to radiobuttons instead of drop down menu

--- src/components/molecules/ThemeSwitch.svelte
+++ src/components/molecules/ThemeSwitch.svelte
@@ -1,18 +1,42 @@
 <script lang="ts">
-    import { theme } from '../../stores';
-</script>
-  
-<!-- TODO: chage this to radiobuttons and then style them correctly -->
-<select bind:value="{$theme}">
-    <option value="system">System</option>
-    <option value="light">Light</option>
-    <option value="dark">Dark</option>
-    <option value="lightSea">LightSea</option>
-    <option value="darkSea">DarkSea</option>
-    <option value="lightBlue">lightBlue</option>
-    <option value="darkBlue">darkBlue</option>
-</select>
+    import { theme, themes } from '../../stores';
+    console.log(themes);
 
-<style>
+</script>
+
+<h3>Färgtema</h3>
+<div role="radiogroup" class="wrapper">
+    {#each themes as themeName}
+        <div class="radiobutton">
+            <input class="sr-only" type="radio" id={themeName} bind:group={$theme} value={themeName} />
+            <label for={themeName}> {themeName} </label>
+        </div>
+    {/each}
+</div>
+
+
+<style lang="scss">
+    h3 {
+        padding: 0rem var(--padding-small) var(--padding-small);
+    }
+
+    .wrapper {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        
+        .radiobutton {
+            flex-grow: 1;
+            text-align: center;
+            // width: fit-content;
+
+            margin: var(--margin-small);
+            border: var(--border);
+            border-radius: var(--radius-small);
+            padding: 0 var(--padding);
+
+            color: var(--font-color);
+        }
+    }
 
 </style>
