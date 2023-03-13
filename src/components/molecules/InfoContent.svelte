250f4c7ca4d9ccde2a6b99241c8d1414c40b0759
Hampus83 <jhmacgyver@gmail.com>
2023-03-13 11:49:29 +0100

    ok

--- src/components/molecules/InfoContent.svelte
+++ src/components/molecules/InfoContent.svelte
@@ -5,11 +5,11 @@
     import data from '../../content.json';
     import Button from "../atoms/Button.svelte";
     
-    interface Data {
-        heading: string;
-        content: string;
-        links: [];
-    }
+    // interface Data {
+    //     heading: string;
+    //     content: string;
+    //     links: [];
+    // }
 
     let btnText = 'Tillbaka'
 
@@ -90,12 +90,10 @@
     <p>{content}</p>
     <h2>{linkHeading}</h2>
     <div class="link-wrapper">
-        <!-- <Link target='' on:click={set478} linkDescription='4-7-8-andning' linkPath='#' />
-        <Link target='' on:click={setPranayama} linkDescription='Pranayama-andning' linkPath='#' />
-        <Link target='' on:click={setBox} linkDescription='Box-andning' linkPath='#' /> -->
-        {#if activeContent == '478'}
+        {#if activeContent == 'box'}
             {#each links as link}
                 <Link target='' linkDescription={link.description} linkPath={link.url}/>
+                <p>test</p>
             {/each} 
         {:else}
             <Link target='' on:click={set478} linkDescription='4-7-8-andning' linkPath='#' />
