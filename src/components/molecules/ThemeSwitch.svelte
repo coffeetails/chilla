5e44c00abde4b3d8da24dd01c3a8972c4a286ee1
Monica Björk <m.bjork95@gmail.com>
2023-03-13 13:54:52 +0100

    Update ThemeSwitch.svelte

--- src/components/molecules/ThemeSwitch.svelte
+++ src/components/molecules/ThemeSwitch.svelte
@@ -1,12 +1,42 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
     import { theme, themes } from '../../stores';
-    console.log(themes);
+    console.log("themes ", themes);
+
+    let displayThemes: string[] = themes;
+    console.log("1 displayThemes", displayThemes);
+
+    onMount(() => {
+        
+        theme.subscribe(() => {
+            console.log("light theme ", $theme);
+            displayThemes = [];
+            console.log("2 displayThemes", displayThemes);
+            for(let themePick of themes) {
+                if(themePick.includes("light")) {
+                    displayThemes.push(themePick);
+                } else if(themePick.includes("dark")) {
+                    displayThemes.push(themePick);
+                // } else if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                //     displayThemes.push(themePick);
+                //     console.log("system dark theme ", $theme);
+                // } else if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                //     displayThemes.push(themePick);
+                //     console.log("system light theme ", $theme);
+                } else {
+                    console.log("wtf");
+                }
+            }
+            console.log("3 displayThemes", displayThemes);
+        });
+        console.log("4 displayThemes", displayThemes);
+    });
 
 </script>
 
 <h3>Färgtema</h3>
 <div role="radiogroup" class="wrapper">
-    {#each themes as themeName}
+    {#each displayThemes as themeName}
         <div class="radiobutton">
             <input class="sr-only" type="radio" id={themeName} bind:group={$theme} value={themeName} />
             <label for={themeName}> {themeName} </label>
