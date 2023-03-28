e5bd0fd6754923f9537547644103afdc58e7449f
Monica Björk <m.bjork95@gmail.com>
2023-03-28 09:16:27 +0200

    added "about us" section

--- src/routes/omsidan/+page.svelte
+++ src/routes/omsidan/+page.svelte
@@ -8,6 +8,8 @@
         <p>{data.about.content}</p>
         <h2>{data.whyBreathing.heading}</h2>
         <p>{data.whyBreathing.content}</p>
+        <h2>Om oss</h2>
+        <p>Sidan är gjord av <a href="https://github.com/Hampus83">Hampus Egecioglu</a> och <a href="https://github.com/MonBjo">Monica Björk</a> som ett examenarbete 2023 för utbildning till Frontendutvecklare.</p>
     </div>
 
 <style lang="scss">
